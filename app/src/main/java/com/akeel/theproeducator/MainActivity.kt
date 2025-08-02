9a123da8a86abf4cbd9ff147d8d0027e4618fb62
Akeel shafi <akeelshafi20@gmail.com>
2025-08-02 13:12:58 +0530

    Initial commit

---
+++ app/src/main/java/com/akeel/theproeducator/MainActivity.kt
@@ -0,0 +1,42 @@
+package com.akeel.theproeducator
+
+import android.os.Bundle
+import android.webkit.WebView
+import android.webkit.WebViewClient
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_main)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+
+        val webView = findViewById<WebView>(R.id.webView)
+        webViewSetup(webView)
+    }
+
+    private fun webViewSetup(webView: WebView) {
+        webView.webViewClient = WebViewClient()
+
+        webView.apply {
+            loadUrl("https://www.theproeducator.com/")
+            settings.javaScriptEnabled = true
+            settings.safeBrowsingEnabled = true
+        }
+    }
+    override fun onBackPressed() {
+        if (webView.canGoBack()) {
+            webView.goBack()
+        } else {
+            super.onBackPressed()
+        }
+    }
+}
